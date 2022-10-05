f304bdc1911db962c9f5c315862ed1daa222a904
Marco <kuan.n.marco@gmail.com>
2022-10-04 19:01:09 -0700

    add fn get_tuple; get_proc_list returns Vec<tuple> holding proc data

--- src/proc.rs
+++ src/proc.rs
@@ -1,19 +1,25 @@
-use std::collections::BTreeMap;
-
 use windows::Win32::{
     Foundation::{CloseHandle, BOOL, CHAR, HANDLE},
-    System::Diagnostics::ToolHelp::{
-        CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32, TH32CS_SNAPPROCESS,
+    System::{
+        Diagnostics::ToolHelp::{
+            CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32,
+            TH32CS_SNAPPROCESS,
+        },
+        Threading::{OpenProcess, PROCESS_VM_OPERATION, PROCESS_VM_WRITE},
     },
 };
 
 // Source example provided by Microsoft: https://learn.microsoft.com/en-us/windows/win32/toolhelp/taking-a-snapshot-and-viewing-processes
 // Traverses through the process list
-pub fn get_proc_list() -> BTreeMap<String, u32> {
-    // B-TreeMap used to store (exe_name, process_id), sorted in alphabetical order
-    let mut exe_id_map: BTreeMap<String, u32> = BTreeMap::new();
+pub fn get_proc_list() -> Vec<(String, u32, HANDLE)> {
+    // Return variable: Stores all the necessary data for read/write ("Database" of our process)
+    let mut proc_database: Vec<(String, u32, HANDLE)> = Vec::new();
+    // tuple that stores (process name, process id, process handle)
+    let mut proc_tup: (String, u32, HANDLE);
     // Holds the process information of each
     let mut pe_32: PROCESSENTRY32;
+    // The handle to the process (given write access)
+    let mut h_proc: HANDLE;
     // Stores a snapshot of all the running processes
     let h_proc_snap: HANDLE;
 
@@ -58,8 +64,25 @@ pub fn get_proc_list() -> BTreeMap<String, u32> {
                 .map(|x| x.0 as char)
                 .collect::<String>();
 
-            // Store (exe_str, process_id) into "exe_id_map" B-TreeMap
-            exe_id_map.insert(exe_str, pe_32.th32ProcessID);
+            // Obtain access to the process handle
+            h_proc = match OpenProcess(
+                PROCESS_VM_WRITE | PROCESS_VM_OPERATION,
+                false,
+                pe_32.th32ProcessID,
+            ) {
+                Ok(handle_proc) => handle_proc,
+                Err(_) => {
+                    // If a process failed to open, it means it might have higher access rights than this program
+                    Process32Next(h_proc_snap, &mut pe_32);
+                    continue;
+                }
+            };
+
+            // Initialize our tuple with (process name, process id, process handle)
+            proc_tup = (exe_str, pe_32.th32ProcessID, h_proc);
+
+            // Store our tuple into our "return variable"/database
+            proc_database.push(proc_tup);
 
             // End the loop when the whole snapshot has been traversed
             if Process32Next(h_proc_snap, &mut pe_32).eq(&BOOL(0i32)) {
@@ -71,6 +94,14 @@ pub fn get_proc_list() -> BTreeMap<String, u32> {
         CloseHandle(h_proc_snap);
     }
 
-    // Return the BTreeMap
-    exe_id_map
+    // Return the database
+    proc_database
+}
+
+// Traverse through the vector containing the tuples from get_proc_list and return the tuple from the pid
+pub fn get_tuple<'a>(
+    list: &'a Vec<(String, u32, HANDLE)>,
+    desired_pid: &u32,
+) -> &'a (String, u32, HANDLE) {
+    list.iter().find(|tup| (*tup).1 == *desired_pid).unwrap()
 }
