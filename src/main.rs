388f2d50df5e74f9c79e31df86f677d05736e1d7
Marco <kuan.n.marco@gmail.com>
2022-10-04 22:05:09 -0700

    Runs a loop on lib::get_cmd(); terminal works

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use read_write_procmem_windows::proc::get_proc_list;
+use windows::Win32::Foundation::HANDLE;
 
+// Main function that will ask the user for their input (command) in a loop and
+// proceeds to read/write/(get a handle) to the process
 fn main() {
-    println!("{:#?}", get_proc_list());
+    let mut id_handle_tup = (&mut 0u32, &mut HANDLE(0));
+
+    loop {
+        read_write_procmem_windows::get_cmd(&mut id_handle_tup.0, &mut id_handle_tup.1);
+    }
 }
